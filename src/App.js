4da6568203aabf4c43ff12455e1a87dd2046bca7
jiyy25 <jiyy825@gmail.com>
2024-01-25 14:25:39 +0900

    보일러 플레이트 완공/ 플러그인 인스톨 및 src,public소스 분리저장

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/Header";
+import Swiper from "./component/Swiper";
+import { Route, Routes } from "react-router-dom";
+import datasrc from './data/osulloc.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <Swiper datasrc={datasrc.swiper}></Swiper>
+    </>
   );
 }
 
